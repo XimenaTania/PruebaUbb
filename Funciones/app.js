42fd55210ae6b91bc7e11debb94211f23260d3cd
XimenaTania <85331667+XimenaTania@users.noreply.github.com>
2025-01-10 11:57:58 -0400

    Add files via upload

---
+++ Funciones/app.js
@@ -0,0 +1,63 @@
+/* Renderizado generico */
+window.onload = () => {
+  document.querySelector("header").innerHTML = `
+  <div class="header-escritorio">
+
+    <a title="UBB" href="#top">
+   <img class="header-logo" src="./ImgeIEBLA/UBB4.png" alt="Logo de UBB" 
+       alt="Logo de UBB" width="100" height="50" />
+    </a>
+    <nav class="header-nav" >
+      <ul>
+        <li><a href="#top">Inicio</a></li>
+        <li><a href="#images">Imagenes</a></li>
+        <li><a href="#info">Noticias</a></li>
+         <li><a href="#fili">Filiales</a></li>
+         
+      </ul>
+    </nav>
+    <button class="menu-hamburguesa" href="" onclick="precionarInterruptor()">
+      <i class="menu-hamburguesa-icono fa-solid fa-bars"></i>
+    </button>
+  </div>
+  <nav class="menu-hamburguesa-nav">
+    <ul>
+      <li><a href="#top">Inicio</a></li>
+      <li><a href="#images">Imagenes</a></li>
+      <li><a href="#info">Noticias</a></li>
+      <li><a href="#fili">Filiales</a></li>
+    </ul>
+  </nav>
+  `;
+
+  document.querySelector(".contacto").innerHTML =`
+   <img src="./ImgeIEBLA/Jesus transformación y vida(1) 1.png"  >
+  <div class="contacto-info">
+    <h1>¡Seguinos en nuestras redes sociales!</h1>
+    <div>
+      <a target="_blank" href="https://www.instagram.com/conectarlabmendoza/"><i class="rosado fa-brands fa-square-instagram"></i></a>
+      <a target="_blank" href="https://www.facebook.com/conectarlabmendoza/"><i class="celeste fa-brands fa-square-facebook"></i></a>
+    </div>
+  </div>
+  `;
+
+  document.querySelector("footer").innerHTML = `
+  <div>
+    <img title="Ciencias" class="footer-logo" src="./logosConectar/logoCiencia.png" alt="Logo ciencia">
+    <img title="Estudio" class="footer-logo" src="./logosConectar/logoEstudio.png" alt="Logo estudio">
+    <img title="Espacio de escuelas" class="footer-logo" src="./logosConectar/logoExplo.png" alt="Logo explo">
+    <img title="Fabricación" class="footer-logo" src="./logosConectar/logoFabricacion.png" alt="Logo fabricacion">
+    <img title="Mini" class="footer-logo" src="./logosConectar/logoMini.png" alt="Logo mini">
+    <img title="Multimedia" class="footer-logo" src="./logosConectar/logoMultimedia.png" alt="Logo logoMultimedia">
+    <img title="Sonido" class="footer-logo" src="./logosConectar/logoSonido.png" alt="Logo sonido">
+  </div>
+  <a title="UBB" href="#top">
+  <img class="header-logo" src="./ImgeIEBLA/UBB4.png"  >
+</a>
+`;
+};
+
+function precionarInterruptor() {
+  document.querySelector(".menu-hamburguesa-nav").classList.toggle("visible");
+  document.querySelector(".menu-hamburguesa-icono").classList.toggle("fa-bars-staggered");
+}
